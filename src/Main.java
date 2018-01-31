89edffcba4625b54bbb6e52305e0d7b49d769ec0
matthias4217 <matthias.goffette@telecom-sudparis.eu>
2018-01-31 16:44:04 +0100

    Added first javadocs

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 
 public class Main {
 
+	/**
+	 * Main function
+	 * <p>
+	 * Main function of the program
+	 * <p>
+	 * @param Null
+	 * @return Null
+	 */
 	public static void main(String[] args) {
 		System.out.println("Quel beau jeu !");
 	}
--- src/gameobjects/Player.java
+++ src/gameobjects/Player.java
@@ -2,9 +2,27 @@ package gameobjects;
 
 import java.util.LinkedList;
 
+/**
+ * 
+ * @author matthias
+ *
+ */
 public class Player {
+	
+	/**
+	 * Health of the Player
+	 */
 	int hp;
+	/**
+	 * Weapon of the Player
+	 */
 	Weapon weapon;
+	/**
+	 * List of States of the Player
+	 */
 	LinkedList<State> states;
+	/**
+	 * Boolean : true for debug state ; false for normal use
+	 */
 	boolean debug;
 }
