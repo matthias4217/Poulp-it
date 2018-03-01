38d9f977d88d5439756a41b268ece97035fd371e
Raphael Tran <raphael.tran@telecom-sudparis.eu>
2018-03-01 12:07:20 +0100

    d

--- src/core/util/CollisionBounds.java
+++ src/core/util/CollisionBounds.java
@@ -3,12 +3,12 @@ package core.util;
 import core.exceptions.InvalidArgumentsException;
 
 /**
- * Bounds used for collisions.
+ * This class represents a collision mask.
  * In practice, this is simply a list of points which define a polygon.
  * If you want rectangular bounds, use the subclass CollisionBox.
  * The user is trusted to create valid forms.
  * 
- * Note: A line can be valid.
+ * Note: A line (two points) is valid.
  * 
  * @author Raph
  *
@@ -29,7 +29,7 @@ public class CollisionBounds {
 	/* Constructor */
 	public CollisionBounds(Vector2[] pointsArray) throws InvalidArgumentsException {
 		if (pointsArray.length < 2) {
-			throw new InvalidArgumentsException("The array of points for a CollisionBox must be at least two");
+			throw new InvalidArgumentsException("The array of points for a CollisionBounds must be at least two");
 		}
 		
 		this.pointsArray = pointsArray;
