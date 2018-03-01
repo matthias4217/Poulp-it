66976251fff49507e1a15e529ebb60009914d945
matthias4217 <matthias.goffette@telecom-sudparis.eu>
2018-03-01 12:47:27 +0100

    CollisionBounds & CollisionBox
      * Ajout d'une méthode setPointsArray à CollisionBounds
      * Ajout d'un constructeur vide à CollisionBounds
      * Modification du constructeur de CollisionBox
      * Modification de test/Draft pour tester

--- src/core/util/CollisionBounds.java
+++ src/core/util/CollisionBounds.java
@@ -7,34 +7,41 @@ import core.exceptions.InvalidArgumentsException;
  * In practice, this is simply a list of points which define a polygon.
  * If you want rectangular bounds, use the subclass CollisionBox.
  * The user is trusted to create valid forms.
- * 
+ *
  * Note: A line can be valid.
- * 
+ *
  * @author Raph
  *
  */
 public class CollisionBounds {
-	
+
 	private Vector2[] pointsArray;
 	public Vector2 getPoint(int i) {
 		return pointsArray[i];
 	}
-	
+	public void setPointsArray(Vector2[] pointsArray) {
+		this.pointsArray = pointsArray;
+	}
+
 	private int nbPoints;
 	public int getNbPoints() {
 		return nbPoints;
 	}
-	
-	
+
+
 	/* Constructor */
 	public CollisionBounds(Vector2[] pointsArray) throws InvalidArgumentsException {
 		if (pointsArray.length < 2) {
 			throw new InvalidArgumentsException("The array of points for a CollisionBox must be at least two");
 		}
-		
+
 		this.pointsArray = pointsArray;
 		nbPoints = pointsArray.length;
 	}
 
-	
+	public CollisionBounds() {
+
+	}
+
+
 }
--- src/test/Draft.java
+++ src/test/Draft.java
@@ -1,33 +1,40 @@
 package test;
 
+import core.exceptions.InvalidArgumentsException;
 import core.util.*;
 
 /**
  * This class is simply used as a draft to try out various things.
- * 
+ *
  * @author Raph
  *
  */
 public class Draft {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws InvalidArgumentsException {
+
 		Vector2 test = new Vector2(1, 1);
 		System.out.println(test.norm());
 		Vector2 test2 = test.normalize();
 		System.out.println(test2);
 		System.out.println(test2.norm());
-		
-		
-		
-		
+
+		Vector2[] truc_t = new Vector2[2];
+		truc_t[0] = test;
+		truc_t[1] = test2;
+
+		CollisionBounds truc = new CollisionBounds(truc_t);
+		System.out.println(truc);
+
+
+
 	}
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
 
 }
 
