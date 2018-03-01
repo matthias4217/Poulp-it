854ead158a9ff4b1f37ba0c95d5d2c5a3d444540
Raphael Tran <raphael.tran@telecom-sudparis.eu>
2018-03-01 09:48:58 +0100

    d

--- src/test/Draft.java
+++ src/test/Draft.java
@@ -11,12 +11,6 @@ import core.util.Vector2;
 public class Draft {
 	public static void main(String[] args) {
 		
-		Vector2 mep = new Vector2(1, 2);
-		System.out.println(mep);
-		
-		Vector2 mep2 = Vector2.multiply(12, mep);
-		System.out.println(mep2);
-		
 		
 		
 		
