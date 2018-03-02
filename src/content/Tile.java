ebe5c75b2b0eaab2f73084431d368d9c3d5c0fae
Raphael Tran <raphael.tran@telecom-sudparis.eu>
2018-03-02 20:16:15 +0100

    Petite amélioration de l'objet Tile

--- src/content/Tile.java
+++ src/content/Tile.java
@@ -1,6 +1,3 @@
-/**
- *
- */
 package content;
 
 import javafx.scene.canvas.GraphicsContext;
@@ -12,42 +9,66 @@ import core.util.Vector2;
  *
  */
 public class Tile {
+	
+	/**
+	 * The size of the side of a tile in the gamespace.
+	 * This constant makes the link between the float coordinates used by the game engine and the discrete nature of tiles.
+	 */
+	public static final float TILE_SIZE = 10f;
+	
+	
+	
+	
 	/*
 	 * It can be converted to the position of non-tile objects by multiplying
-	 * it by TILESIZE, which will be defined somewhere eventually
+	 * it by TILE_SIZE, which IS defined HERE
 	 */
-	public Vector2 position;
+	private Vector2 position;
 
-	/* it is linked to an image, but the image must not be duplicated
+	/* It is linked to an image, but the image must not be duplicated
 	 * for now I don't care, just to test the implementation
 	 */
 	public ImageView sprite;
+	
+	/**
+	 * The type of tile (full block, slope, etc)
+	 */
 	public TileType type;
-	public int tilesize;
 
-	public Tile(Vector2 position, ImageView imageview, int tilesize, TileType type) {
-		this.position = position;
-		this.sprite = imageview;
-		this.tilesize = tilesize;
+	
+	public Tile(int xInt, int yInt, ImageView imageView, TileType type) {
+		this.position = new Vector2(TILE_SIZE * xInt, TILE_SIZE * yInt);
+		this.sprite = imageView;
 		this.type = type;
 	}
 
+	
 	public void render(GraphicsContext gc) {
 		/* Render this Sprite on the GraphicsContext gc.
 		 * It may be optimized if we don't do a getImage()
 		 */
-		System.out.println("x: " + position.x * tilesize + ", y: " + position.y * tilesize);
-		gc.drawImage(sprite.getImage(), position.x * tilesize,
-				position.y * tilesize);
+		System.out.println("x: " + position.x + ", y: " + position.y);
+		gc.drawImage(sprite.getImage(), position.x,	position.y);
 	}
 
+	
 	public String toString() {
 		return type.toString() + " : " + position.toString();
 	}
 
+
+	
+	
+	
 	/*
 	 * List of the values type can be
 	 */
-	public enum TileType { SQUARE, TRIANGLE_UP_RIGHT, TRIANGLE_UP_LEFT,
-		TRIANGLE_DOWN_RIGHT, TRIANGLE_DOWN_LEFT}
+	public enum TileType {
+		SQUARE,
+		TRIANGLE_UP_RIGHT,
+		TRIANGLE_UP_LEFT,
+		TRIANGLE_DOWN_RIGHT,
+		TRIANGLE_DOWN_LEFT,
+		
+	}
 }
--- src/levels/Level0.java
+++ src/levels/Level0.java
@@ -6,7 +6,6 @@ package levels;
 
 import content.Tile;
 import content.Tile.TileType;
-import core.util.Vector2;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -22,7 +21,7 @@ public class Level0 {
 	public Level0() {
 		Image tile_down = new Image("resources/tile-down.png");
 		ImageView iv_tile_down = new ImageView(tile_down);
-		tiles[0] = new Tile(new Vector2(0, 0), iv_tile_down, 32, TileType.SQUARE);
-		tiles[1] = new Tile(new Vector2(1, 1), iv_tile_down, 32, TileType.SQUARE);
+		tiles[0] = new Tile(0, 0, iv_tile_down, TileType.SQUARE);
+		tiles[1] = new Tile(1, 1, iv_tile_down, TileType.SQUARE);
 	}
 }
