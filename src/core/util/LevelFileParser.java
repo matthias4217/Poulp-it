a98be3d51fc3016f148e5268f1babb8c39c37eae
matthias4217 <matthias.goffette@telecom-sudparis.eu>
2018-03-05 23:12:53 +0100

    Finition de la conexion des textures
      * L'issue #27 est maintenant résolue
      * Les tiles se connectent de bonne manière
      * Le code n'est pas très beau/digeste, il faudrait probablement le revoir

--- src/core/util/LevelFileParser.java
+++ src/core/util/LevelFileParser.java
@@ -8,6 +8,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
 
 import content.Tile;
@@ -154,6 +155,7 @@ public class LevelFileParser {
 		// We get the TILE_SIZE
 		float tilesize = Tile.TILE_SIZE;
 		//System.out.println(theme);
+
 		//SQUARE
 		ImageView tilePlain = new ImageView(new Image("resources/tiles/"
 				 + theme + "/tile-plain.png", tilesize, tilesize, false, false));
@@ -167,8 +169,18 @@ public class LevelFileParser {
 		ImageView tileSurfaceRight = new ImageView(new Image("resources/tiles/"
 				 + theme + "/tile-surface-right.png", tilesize, tilesize, false, false));
 		// surface-double
+		ImageView tileSurfaceDoubleDownLeft = new ImageView(new Image("resources/tiles/"
+		 + theme + "/tile-surface-double-down-left.png", tilesize, tilesize, false, false));
 		ImageView tileSurfaceDoubleDownRight = new ImageView(new Image("resources/tiles/"
-		 + theme + "/tile-surface-double-down-right.png", tilesize, tilesize, false, false));
+				 + theme + "/tile-surface-double-down-right.png", tilesize, tilesize, false, false));
+		ImageView tileSurfaceDoubleLeftRight = new ImageView(new Image("resources/tiles/"
+				 + theme + "/tile-surface-double-left-right.png", tilesize, tilesize, false, false));
+		ImageView tileSurfaceDoubleTopDown = new ImageView(new Image("resources/tiles/"
+				 + theme + "/tile-surface-double-top-down.png", tilesize, tilesize, false, false));
+		ImageView tileSurfaceDoubleTopLeft = new ImageView(new Image("resources/tiles/"
+				 + theme + "/tile-surface-double-top-left.png", tilesize, tilesize, false, false));
+		ImageView tileSurfaceDoubleTopRight = new ImageView(new Image("resources/tiles/"
+				 + theme + "/tile-surface-double-top-right.png", tilesize, tilesize, false, false));
 		// surface-triple
 		ImageView tileSurfaceTripleDown = new ImageView(new Image("resources/tiles/"
 				 + theme + "/tile-surface-triple-down.png", tilesize, tilesize, false, false));
@@ -181,6 +193,7 @@ public class LevelFileParser {
 		// surface-quadruple
 		ImageView tileSurfaceQuadruple = new ImageView(new Image("resources/tiles/"
 		 + theme + "/tile-surface-quadruple.png", tilesize, tilesize, false, false));
+
 		// TRIANGLE
 		ImageView tileTriangleDownRight = new ImageView(new Image("resources/tiles/"
 				 + theme + "/triangle-down-right.png", tilesize, tilesize, false, false));
@@ -196,6 +209,7 @@ public class LevelFileParser {
 		List<Tile> list_tiles = new ArrayList<Tile>();
 		// the following tile won't be used, it's just so that the IDE won't yell
 		Tile tile = new Tile(0, 0, tileSurfaceTop, TileType.SQUARE);
+
 		for (int i = 0; i < max_i + 1; i++) {
 			for (int j = 0; j< max_j + 1; j++) {
 				char c = level_text[i][j];
@@ -219,8 +233,7 @@ public class LevelFileParser {
 					 * case 2: double
 					 * case 3: surface
 					 */
-					// for now, all will be the same
-					tile = new Tile(j, i, tileSurfaceTop, TileType.SQUARE);
+
 					int nbrDirectNeighbours = 0;
 					// So, we make a list/array of neighbours
 					List<neighbourPosition> neighbours = new ArrayList<neighbourPosition>();
@@ -249,10 +262,10 @@ public class LevelFileParser {
 					        	nbrDirectNeighbours ++;
 					        	// then we detect where the neighbours are
 					        	if (curRow > i) {
-					        		neighbours.add(neighbourPosition.UP);
+					        		neighbours.add(neighbourPosition.DOWN);
 					        	}
 					        	else if (curRow < i) {
-					        		neighbours.add(neighbourPosition.DOWN);
+					        		neighbours.add(neighbourPosition.UP);
 					        	}
 					        	else if (curCol < j) {
 					        		neighbours.add(neighbourPosition.LEFT);
@@ -302,30 +315,77 @@ public class LevelFileParser {
 						 * Not a hard problem for the textures, we just need to make 6 cases
 						 */
 						tile = new Tile(j, i, tileSurfaceDoubleDownRight, TileType.SQUARE);
+						List<neighbourPosition> temp2 = new ArrayList<neighbourPosition>(4);
+						temp2.add(neighbourPosition.UP);
+						temp2.add(neighbourPosition.DOWN);
+						temp2.add(neighbourPosition.LEFT);
+						temp2.add(neighbourPosition.RIGHT);
+						for (int nIndex = 0; nIndex < 2; nIndex++) {
+							temp2.remove(neighbours.get(nIndex));
+						}
+						if ((temp2.get(0) == neighbourPosition.UP
+								&& temp2.get(1) == neighbourPosition.DOWN)
+								|| (temp2.get(0) == neighbourPosition.DOWN
+										&& temp2.get(1) == neighbourPosition.UP)) {
+
+							tile = new Tile(j, i, tileSurfaceDoubleTopDown, TileType.SQUARE);
+						}
+						else if ((temp2.get(0) == neighbourPosition.UP
+								&& temp2.get(1) == neighbourPosition.RIGHT)
+								|| (temp2.get(0) == neighbourPosition.RIGHT
+										&& temp2.get(1) == neighbourPosition.UP)) {
+
+							tile = new Tile(j, i, tileSurfaceDoubleTopRight, TileType.SQUARE);
+						}
+						else if ((temp2.get(0) == neighbourPosition.UP
+								&& temp2.get(1) == neighbourPosition.LEFT)
+								|| (temp2.get(0) == neighbourPosition.LEFT
+										&& temp2.get(1) == neighbourPosition.UP)) {
+
+							tile = new Tile(j, i, tileSurfaceDoubleTopLeft, TileType.SQUARE);
+						}
+						else if ((temp2.get(0) == neighbourPosition.DOWN
+								&& temp2.get(1) == neighbourPosition.RIGHT)
+								|| (temp2.get(0) == neighbourPosition.RIGHT
+										&& temp2.get(1) == neighbourPosition.DOWN)) {
+
+							tile = new Tile(j, i, tileSurfaceDoubleDownRight, TileType.SQUARE);
+						}
+						else if ((temp2.get(0) == neighbourPosition.DOWN
+								&& temp2.get(1) == neighbourPosition.LEFT)
+								|| (temp2.get(0) == neighbourPosition.LEFT
+										&& temp2.get(1) == neighbourPosition.DOWN)) {
+
+							tile = new Tile(j, i, tileSurfaceDoubleDownLeft, TileType.SQUARE);
+						}
+						if ((temp2.get(0) == neighbourPosition.RIGHT
+								&& temp2.get(1) == neighbourPosition.LEFT)
+								|| (temp2.get(0) == neighbourPosition.LEFT
+										&& temp2.get(1) == neighbourPosition.RIGHT)) {
+
+							tile = new Tile(j, i, tileSurfaceDoubleLeftRight, TileType.SQUARE);
+						}
 						break;
 					case 3:
-						List<neighbourPosition> temp = new ArrayList<neighbourPosition>(4);
-						temp.add(neighbourPosition.UP);
-						temp.add(neighbourPosition.DOWN);
-						temp.add(neighbourPosition.LEFT);
-						temp.add(neighbourPosition.RIGHT);
+						//List<neighbourPosition> temp3 = new ArrayList<neighbourPosition>(4);
+						EnumSet<neighbourPosition> temp3 = EnumSet.of(
+								neighbourPosition.UP, neighbourPosition.DOWN,
+								neighbourPosition.LEFT, neighbourPosition.RIGHT);
 						for (int nIndex = 0; nIndex < 3; nIndex++) {
-							temp.remove(neighbours.get(nIndex));
+							temp3.remove(neighbours.get(nIndex));
 						}
-						switch (temp.get(0)) {
-						case UP:
+						if (temp3.contains(neighbourPosition.UP)) {
 							tile = new Tile(j, i, tileSurfaceTop, TileType.SQUARE);
-							break;
-						case DOWN:
+						}
+						else if (temp3.contains(neighbourPosition.DOWN)) {
 							tile = new Tile(j, i, tileSurfaceDown, TileType.SQUARE);
-							break;
-						case LEFT:
+						}
+						else if (temp3.contains(neighbourPosition.LEFT)) {
 							tile = new Tile(j, i, tileSurfaceLeft, TileType.SQUARE);
-							break;
-						case RIGHT:
+						}
+						else if (temp3.contains(neighbourPosition.RIGHT)) {
 							tile = new Tile(j, i, tileSurfaceRight, TileType.SQUARE);
-							break;
-					}
+						}
                                                 /*
                                                  * xxx
                                                  * xX
