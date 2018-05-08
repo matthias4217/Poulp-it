e7201c1d562538a69bec3118904cfcb64a8443a6
Raphael Tran <raphael.tran@telecom-sudparis.eu>
2018-05-08 16:46:10 +0200

    fdghgh

--- src/core/GameEngine.java
+++ src/core/GameEngine.java
@@ -173,7 +173,7 @@ public class GameEngine {
 			Vector2 spawnPosition = new Vector2((float)Launcher.WINDOW_WIDTH / 2, (float) Launcher.WINDOW_HEIGHT / 2);
 			spawnPosition.translate(Vector2.RIGHT().multiply(100 * i));
 //			spawnPosition = new Vector2(280, 710);
-			spawnPosition = new Vector2(585, 710);
+			spawnPosition = new Vector2(585, 730);
 			Player playerI = new Player(spawnPosition, 10);
 			players[i] = playerI;
 			allGameObjects.add(playerI);
