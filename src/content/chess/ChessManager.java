ad7a49b9a71272533b2919986faaea29c0d6069e
Raphael Tran <raphael.tran@telecom-sudparis.eu>
2018-06-06 09:41:08 +0200

    Now thinking

---
+++ src/content/chess/ChessManager.java
@@ -0,0 +1,22 @@
+package content.chess;
+
+import core.PlayerInput;
+import content.GameManager;
+
+/**
+ * 
+ * 
+ * @author Raph
+ *
+ */
+public class ChessManager implements GameManager {
+
+	@Override
+	public void apply(float deltaTime, PlayerInput gameInformation, PlayerInput previousPlayerInput) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+
+}
--- src/content/chess/pieces/ChessPiece.java
+++ src/content/chess/pieces/ChessPiece.java
@@ -1,6 +1,11 @@
 package content.chess.pieces;
 
 import content.GameObject;
+import content.Layer;
+import content.Tag;
+import core.util.Collider;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 
 /**
  * The superclass for all chess pieces.
@@ -10,9 +15,47 @@ import content.GameObject;
  */
 public abstract class ChessPiece extends GameObject {
 
+	public int x;
+	public int y;
+
+
+	public ChessPiece(int x, int y, Image sprite) {
+		super(null,
+				sprite,
+				Layer.DEFAULT,
+				Tag.DEFAULT,
+				Collider.NO_COLLIDER());
+
+		this.x = x;
+		this.y = y;
+	}
+
+
+
 	
 	
-	
-	
-	
+	/**
+	 * Move this piece to the new coordinates (x, y)
+	 * 
+	 * @param x
+	 * @param y
+	 */
+	public void move(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+
+
+
+	@Override
+	public void render(GraphicsContext gc, double windowWidth, double windowHeight) {
+
+
+
+
+	}
+
+
+
 }
